ed4daece254f5d7647bae94358dbe571a4236b82
txstu09 <sgillies09@gmail.com>
2017-12-19 21:13:47 -0600

    question 2 all

--- sql-homework.sql
+++ sql-homework.sql
@@ -11,4 +11,29 @@ SELECT UPPER(CONCAT_WS(' ', first_name, last_name))
 AS Actor_Name
 FROM actor;
 
-# 
+# 2a
+# ---------------------------
+SELECT actor_id, first_name, last_name
+FROM actor
+WHERE first_name = 'Joe';
+
+# 2b
+# ---------------------------
+SELECT *
+FROM actor
+WHERE last_name LIKE '%gen%';
+
+# 2c
+# ---------------------------
+SELECT last_name, first_name
+FROM actor
+WHERE last_name LIKE '%li%';
+
+# 2d
+# ---------------------------
+SELECT country_id, country
+FROM country
+WHERE country IN ('Afghanistan','Bangladesh','China');
+
+# 3a
+# ---------------------------
